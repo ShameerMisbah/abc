4ac77c0eb39b639c6a6e8a8a2a15101039deef04
ShameerMisbah <shameermisbah@gmail.com>
2026-04-27 00:55:44 +0530

    Update default SESSION_ID in settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'd29eb03f08544f00' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '571b22ea2ea7a949' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
